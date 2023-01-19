7693dd1fa76f4a4c509f6694b83dc3f02406b15b
saxtuchi <xx.miam.xx25@gmail.com>
2023-01-20 02:12:08 +0900

    itemモデル単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,12 +4,14 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :info, presence: true
-  validates :category_id , numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :shipping_free_status_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :price, presence: true
+  validates :category_id , numericality: { other_than: 1 , message: "can't be blank" }
+  validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_free_status_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank" }
+  with_options presence: true, format: { with: /\A[0-9]+\z/, message: "半角文字を使用してください" } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, presence: { message: "can't be blank" }
+  end
   validates :image, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Team.name}
+    info {'テスト'}
+    category {Category.all.sample}
+    sales_status {SalesStatus.all.sample}
+    shipping_free_status {ShippingFreeStatus.all.sample}
+    prefecture {Prefecture.all.sample}
+    scheduled_delivery {ScheduledDelivery.all.sample}
+    price {'1000'}
+    association :user 
+  
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
